a006a926bd8edf8ac98de1810909910f4c4e03e9
Maria Romanchuk <mashulya_r_88@mail.ru>
2023-03-01 21:15:48 +0700

    Initial Commit

---
+++ UsersFriends/UsersFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersFriendsApp.swift
+//  UsersFriends
+//
+//  Created by Мария Романчук on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct UsersFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
